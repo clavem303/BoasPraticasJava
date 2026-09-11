7f54054a5391703560af1b68b63d0ce35d555af3
c139717 <marcondes.araujo@caixa.gov.br>
2026-09-11 10:54:49 -0300

    create test for ShelterService

--- src/test/java/tech/clavem303/service/ShelterServiceTest.java
+++ src/test/java/tech/clavem303/service/ShelterServiceTest.java
@@ -1,9 +1,9 @@
 package tech.clavem303.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tech.clavem303.client.ClientHttpConfiguration;
-import tech.clavem303.domain.Shelter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -12,35 +12,72 @@ import java.net.http.HttpResponse;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ShelterServiceTest {
-    private final ClientHttpConfiguration client = mock(ClientHttpConfiguration.class);
-    private ShelterService shelterService = new ShelterService(client);
-    private HttpResponse<String> response = mock(HttpResponse.class);
-    private Shelter shelter = new Shelter("Scooby", "19999999999", "scooby@scooby.com");
+
+    private final ClientHttpConfiguration client =
+            mock(ClientHttpConfiguration.class);
+
+    private final ShelterService shelterService =
+            new ShelterService(client);
+
+    @SuppressWarnings("unchecked")
+    private final HttpResponse<String> response =
+            mock(HttpResponse.class);
+
+    private final PrintStream saidaOriginal = System.out;
+
+    @AfterEach
+    public void restaurarSaidaPadrao() {
+        System.setOut(saidaOriginal);
+    }
 
     @Test
-    public void shouldCallServiceWhenGetRequestIsMade () throws IOException, InterruptedException {
-        shelter.setId(0L);
+    public void shouldCallServiceWhenGetRequestIsMade()
+            throws IOException, InterruptedException {
+
         String expectedSheltersRegistered = "Abrigos cadastrados:";
         String expectedIdAndName = "0 - Scooby";
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        String json = """
+                [
+                    {
+                        "id": 0,
+                        "nome": "Scooby",
+                        "telefone": "19999999999",
+                        "email": "scooby@scooby.com"
+                    }
+                ]
+                """;
+
+        when(response.body()).thenReturn(json);
+        when(client.triggerGetRequest(anyString())).thenReturn(response);
+
+        ByteArrayOutputStream byteArrayOutputStream =
+                new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(byteArrayOutputStream);
         System.setOut(printStream);
 
-        when(response.body()).thenReturn("[{+shelter.toString()+}]");
-        when(client.triggerGetRequest(anyString())).thenReturn(response);
-
         shelterService.listShelters();
 
-        String[] lines = byteArrayOutputStream.toString().split(System.lineSeparator());
+        String[] lines = byteArrayOutputStream
+                .toString()
+                .split(System.lineSeparator());
+
         String actualShelterRegistered = lines[0];
         String actualIdAndName = lines[1];
 
-        Assertions.assertEquals(expectedSheltersRegistered, actualShelterRegistered);
-        Assertions.assertEquals(expectedIdAndName, actualIdAndName);
-    }
+        Assertions.assertEquals(
+                expectedSheltersRegistered,
+                actualShelterRegistered
+        );
+        Assertions.assertEquals(
+                expectedIdAndName,
+                actualIdAndName
+        );
 
+        verify(client).triggerGetRequest(anyString());
+    }
 }
